c7e12a18bf635e161aec3a8a911df11211703ea1
BlackBeard-Teach <39981840+BlackBeard-Teach@users.noreply.github.com>
2018-10-30 16:19:52 +0200

    Add files via upload

---
+++ Config/setup.php
@@ -0,0 +1,92 @@
+<?php
+
+require_once 'database.php';
+
+// CREATE DATABASE
+try {
+        // Connect to Mysql server
+        $dbh = new PDO($DB_DSN1, $DB_USER, $DB_PASSWORD);
+        $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $sql = "CREATE DATABASE IF NOT EXISTS Camagru";
+        $dbh->exec($sql);
+        echo "Database created successfully<br>";
+    } catch (PDOException $e) {
+        echo "ERROR CREATING DB: \n".$e->getMessage();
+        exit(1);
+    }
+
+// CREATE TABLE USERS
+try {
+        // Connect to DATABASE previously created
+        $dbh = new PDO($DB_DSN, $DB_USER, $DB_PASSWORD);
+        $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        //sql to create table
+        $sql = "CREATE TABLE IF NOT EXISTS users (
+          id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+          username VARCHAR(50) NOT NULL,
+          mail VARCHAR(100) NOT NULL,
+          password VARCHAR(255) NOT NULL,
+          token VARCHAR(50) NOT NULL,
+          verified VARCHAR(1) NOT NULL DEFAULT 'N'
+        )";
+        $dbh->exec($sql);
+        echo "Table users created successfully<br>";
+    } catch (PDOException $e) {
+        echo "ERROR CREATING TABLE: ".$e->getMessage() . "<br>";
+    }
+
+// CREATE TABLE GALLERY
+try {
+        // Connect to DATABASE previously created
+        $dbh = new PDO($DB_DSN, $DB_USER, $DB_PASSWORD);
+        $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $sql = "CREATE TABLE IF NOT EXISTS gallery (
+          id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+          userid INT(11) NOT NULL,
+          img VARCHAR(100) NOT NULL,
+          FOREIGN KEY (userid) REFERENCES users(id)
+        )";
+        $dbh->exec($sql);
+        echo "Table gallery created successfully<br>";
+    } catch (PDOException $e) {
+        echo "ERROR CREATING TABLE: ".$e->getMessage() ."<br>";
+    }
+
+// CREATE TABLE LIKE
+try {
+        // Connect to DATABASE previously created
+        $dbh = new PDO($DB_DSN, $DB_USER, $DB_PASSWORD);
+        $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $sql = "CREATE TABLE IF NOT EXISTS likes (
+          id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+          userid INT(11) NOT NULL,
+          galleryid INT(11) NOT NULL,
+          type VARCHAR(1) NOT NULL,
+          FOREIGN KEY (userid) REFERENCES users(id),
+          FOREIGN KEY (galleryid) REFERENCES gallery(id)
+        )";
+        $dbh->exec($sql);
+        echo "Table like created successfully<br>";
+    } catch (PDOException $e) {
+        echo "ERROR CREATING TABLE: ".$e->getMessage() ."<br>";
+    }
+
+// CREATE TABLE COMMENT
+try {
+        // Connect to DATABASE previously created
+        $dbh = new PDO($DB_DSN, $DB_USER, $DB_PASSWORD);
+        $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $sql = "CREATE TABLE IF NOT EXISTS comment (
+          id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+          userid INT(11) NOT NULL,
+          galleryid INT(11) NOT NULL,
+          comment VARCHAR(255) NOT NULL,
+          FOREIGN KEY (userid) REFERENCES users(id),
+          FOREIGN KEY (galleryid) REFERENCES gallery(id)
+        )";
+        $dbh->exec($sql);
+        echo "Table comment created successfully<br>";
+    } catch (PDOException $e) {
+        echo "ERROR CREATING TABLE: ".$e->getMessage() ."<br>";
+    }
+?>
