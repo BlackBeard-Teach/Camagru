1f53fd97afd523c619ac15b241ad9f2b26ac753a
BlackBeard-Teach <39981840+BlackBeard-Teach@users.noreply.github.com>
2018-11-02 16:40:06 +0200

    Update signup.php
    Added password complexity checking

--- forms/signup.php
+++ forms/signup.php
@@ -1,12 +1,13 @@
 <?php
 session_start();
 
-include_once '../functions/signup.php';
+include_once '../functions/validation.php';
 
 // retrieve values
 $mail = $_POST['email'];
 $username = $_POST['username'];
 $password = $_POST['password'];
+$password1 = $_POST['password'];
 
 $_SESSION['error'] = null;
 
@@ -28,10 +29,42 @@ if (strlen($username) > 50 || strlen($username) < 3) {
   return;
 }
 
-if (strlen($password) < 3) {
-  $_SESSION['error'] = "Password should be between 3 and 255 characters";
+if (strlen($password) < 8) {
+  $_SESSION['error'] = "Password too short! Should be at least 8 characters long";
   header("Location: ../signup.php");
   return;
+}
+
+if ($password != $password1)
+{
+    $_SESSION['error'] = "Password do not match";
+    header("Location: ../signup.php");
+}
+
+if (!preg_match("#[0-9]+#", $password))
+{
+    $_SESSION['error'] = "Password must include at least one number!";
+    header("Location: ../signup.php");
+    return;
+}
+
+if (!preg_match("#[a-zA-Z]+#", $password))
+{
+    $_SESSION['error'] = "Password must include at least onr letter!";
+    header("Location: ../signup.php");
+    return;
+}
+
+if( !preg_match("#\W+#", $password) ) {
+    $_SESSION['error'] = "Password must include at least one symbol!";
+    header("Location: ../signup.php");
+    return;
+}
+
+if (preg_match("#.*^(?=.{8,20})(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*\W).*$#", $password)){
+    echo "Your password is strong.";
+} else {
+    $_SESSION['error'] = "Your password is not secure.";
 }
 
 if (!preg_match("/^[a-zA-Z0-9]*$/", $username)) {
@@ -40,9 +73,10 @@ if (!preg_match("/^[a-zA-Z0-9]*$/", $username)) {
     return;
 }
 
-$url = $_SERVER['HTTP_HOST'] . str_replace("/forms/signup.php", "", $_SERVER['REQUEST_URI']);
+$url = $_SERVER['HTTP_HOST'] . str_replace("/forms/validation.php", "", $_SERVER['REQUEST_URI']);
 
 signup($mail, $username, $password, $url);
 
 header("Location: ../signup.php");
 ?>
+
