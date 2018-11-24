dee38b53abeadbfa218911da7c2d5293ebc8d853
Thamsaqa Xabanisa <42369897+Exabanis@users.noreply.github.com>
2018-11-24 14:13:01 +0200

    Update cam.js

--- cam.js
+++ cam.js
@@ -1,9 +1,9 @@
 (function() {
-    var video = document.getElementById('video'),
+
+    var video = document.querySelector('video'),
     canvas = document.getElementById('canvas'),
     context = canvas.getContext('2d'),
-    photo = document.getElementById('photo'),
-    vendorUrl = window.webkitURL;
+    photo = document.getElementById('photo');
 
     navigator.getMedia = (navigator.getUserMedia || navigator.webkitGetUserMedia ||
                          navigator.moGetUserMedia || navigator.msGetUserMedia);
@@ -12,13 +12,29 @@
         video: true,
         audio: false
     }, function(stream){
-        video.src = vendorUrl.createObjectURL(stream);
+        video.srcObject = stream;
         video.play();
+    
     }, function(){
     });
 
+    //capture image from video
     document.getElementById('capture').addEventListener('click', function(){
+        console.log("Capture and save");
         context.drawImage(video, 0, 0, 400, 300);
-        photo.setAttribute('src', canvas.toDataURL('image/png'));
-    })
+        photo.value = canvas.toDataURL('image/png');
+    });
+
+    //insert emoji
+    document.getElementById('capture1').addEventListener('click', function() {
+        var img_id = document.getElementById("emos").value,
+        img = document.getElementById(img_id);
+        context.drawImage(img,0,0,400,300);
+        photo.value = canvas.toDataURL('image/png');
+    });
+
+    document.getElementById('clear').addEventListener('click', function() {
+        context.clearRect(0, 0, canvas.width, canvas.height);
+    });
+    
 })();
