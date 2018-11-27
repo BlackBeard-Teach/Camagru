3541eb0e03fd8adbef0b280f7e775e2ba99ed7be
BlackBeard-Teach <39981840+BlackBeard-Teach@users.noreply.github.com>
2018-11-27 11:07:40 +0200

    Added a delete button and to view all images
    Can view all images that you own and be able to delete them using the provided button

--- home.php
+++ home.php
@@ -9,17 +9,6 @@
 <div id="head">
     <h2>Welcome to Camagru <?php echo  '<br>'.$_SESSION['username'] ; ?></h2>
    
-
-
-    <!--<a class="signupPopup" href="Logout.php" title="Logout">Logout<span></span></a>
-    <a class="signupPopup" href="edit_profile.php" title="Logout">Edit profile<span></span></a>
-     <a class="signupPopup" href="Gallery.php" title="View Gallery">View Gallery<span></span></a>
-     <form method="post" action="cam.php"><input name="call cam" type="submit" value=" Take Pic "></form>
-     <script src="cam.js"></script>
-
-    </div>
-    <div class="header">-->
-    
     <nav>
         <ul>
         <li><a href="logout.php">Logout</a></li>
@@ -36,7 +25,38 @@
            
         </ul>
     </nav>
-    
+       <?php
+    session_start();
+    require_once('config/database.php');
+
+    $name =$_SESSION['username'];
+    $result;
+
+    try{
+        $conn = new PDO($DB_DSN, $DB_USER, $DB_PASSWORD);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $result = $conn->query("SELECT * FROM `images` WHERE user = '". $name ."' ORDER BY date_added DESC LIMIT 6 ", PDO::FETCH_ASSOC);
+    } catch(PDOException $e){
+        echo "ERROR EXECUTING: \n".$e->getMessage();
+    }
+    ?>
+    <?php
+    if ($result)
+        foreach ($result as $row) {
+            ?><ul><li style="list-style-type: none;"><img id="e1" src=<?= $row['image']; ?> width="40%" height="auto">
+    //Delete button that sends image id to delete_images.php
+                <button>
+                    <a aria-hidden="true"
+                       href="delete_images.php?type=image&image_id=<?php echo $row['image_id']; ?>">
+                        Delete</a>
+                </button>
+            </li></ul>
+
+            <?php
+        }
+    else
+        echo "failure";
+    ?>
 </div> 
  
 </body>
