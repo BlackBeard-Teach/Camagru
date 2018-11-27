d1f731dd4eef90eb86846c9f32e8360e212c78aa
Ace <39917286+Gkganakg@users.noreply.github.com>
2018-11-27 10:27:52 +0200

    email . notification embedded

--- comments.php
+++ comments.php
@@ -6,6 +6,8 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 include 'Config/database.php';
 
+
+
 htmlspecialchars($_SESSION['username']);
 
 	try
@@ -14,41 +16,95 @@ htmlspecialchars($_SESSION['username']);
 		$image_id 		= trim(htmlspecialchars($_POST['image_id']));
 		$comment 		= htmlspecialchars($_POST['comment_txt']);
 
+		echo  ".$Username .$comment .$image_id .fe";
 		if (!isset($Username) || empty($Username))
-		{?>
-            <script type="text/javascript">
-                var r = confirm("You must be logged in to comment on pictures...click OK to log in");
-                if (r == true) {
-                    window.location.href = "login.php";
-                } else {
-                    alert("You can't comment anonymously!");
-                    window.location.href = "gallery.php";
-                }
-            </script>
-
-		<?php}
+		{
+			echo "Invalid Username!! <br>";
+		}
 		else if (!isset($comment) || empty($comment))
-		{?>
-            <script type="text/javascript">
-                alert("Can't post an empty comment!!")
-            </script>
-<?php	}
+		{
+			echo "Invalid comment!! <br>";
+		}
 		else if ((isset($Username) && !empty($Username))
 			&& (isset($image_id) && !empty($image_id))
 			&& (isset($comment) && !empty($comment))) {
-            $dbh = new PDO($DB_DSN, $DB_USER, $DB_PASSWORD);
+            $dbh = new PDO($DSN_dbname, $username, $password);
             $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             // $sql = "USE ".$DB_NAME;
-            $sql = $dbh->prepare("INSERT INTO comments (Username, comment, image_id)
+            $sql = $dbh->prepare("INSERT INTO comments (Commenter, comment, image_id)
 				VALUES (:Username, :comment, :image_id)");
             $sql->execute(array(':Username' => $Username, ':comment' => $comment, ':image_id' => $image_id));
-
+			mailNotif($image_id, $Username);
 			}
 	}
 		catch(PDOException $e)
         {
             echo $sql . "<br>" . $e->getMessage();
-        }
+		}
+	
+		function mailNotif($img_id,$Username) {
+			include ('config/database.php');
+		
+			try {
+				$pdo = new PDO($DSN_dbname,$username,$password);
+				$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+			$wat = $pdo->prepare("SELECT * FROM gallery WHERE img_id = $img_id");
+			$wat->execute();
+			$found = $wat->rowCount();
+			while ($row = $wat->fetch(PDO::FETCH_ASSOC)) {
+				$name  = $row['username'];
+			}
+			if ($found == 1)
+			{
+				checkUser($name, $Username);
+				//header("Location: home.php");
+			}
+			else{
+				header("Location: index.php");
+			}
+		}catch(PDOexception $e)
+		{
+            echo $sql . "<br>" . $e->getMessage();
+		}
+
+	}
+	function checkUser($name, $Username) {
+		include ('config/database.php');
+		try {
+			$pdo = new PDO($DSN_dbname,$username,$password);
+			$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		$wat = $pdo->prepare("SELECT Email FROM users WHERE Username = :username AND mailNotif = :num");
+		$wat->execute(array(':username' => $name, ':num'=> 1));
+		$found = $wat->rowCount();
+		$row = $wat->fetch(PDO::FETCH_ASSOC);
+		$mail = $row['Email'];
+
+		if ($found == 1)
+		{
+			func_mail($Username,$mail);
+		}
+	} catch(PDOexception $e)
+		{
+            echo  $e->getMessage();
+		}
+
+	}
+	function func_mail($username, $mail) {
+  
+		$to      = $mail; // Send email to our user
+		$subject = ' Camagru Signup | Verification'; // Give the email a subject 
+
+		$message = ''.$username.' commented on your picture
+		
+
+		'; // Our message above including the link
+							
+		$headers = 'From:camagruteam@camagru.com' . "\r\n"; // Set from headers
+		mail($to, $subject, $message, $headers); // Send our email
+
+
         header("Location: gallery.php");
-        $conn = null;
+		$conn = null;
+	}
+
 ?>
